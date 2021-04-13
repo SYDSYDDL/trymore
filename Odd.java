091c6fae176dec5320bc9ea337bc20ff96bc8a0e
GTK <62057850+GTK2@users.noreply.github.com>
2021-04-13 16:58:23 +0900

    Create Odd.java

---
+++ Odd.java
@@ -0,0 +1,9 @@
+class Odd{
+  public static void main(String[] args){
+  	// 1~10 사이의 정수 중 홀수를 출력하는 프로그램
+    for(int i = 1; i <=10 ; i++){
+    	if(i % 2 == 0)
+    		System.out.println(i);
+    }
+  }
+}
